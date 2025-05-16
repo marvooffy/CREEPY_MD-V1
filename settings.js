029107221a01a78e4efc4ae9de4ab53ea9d4be69
marvooffy <marvokeyz792@gmail.com>
2025-05-16 10:24:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
-//Created by Danny 
+//Created by Marvo 
 //Dont clone cause if i catch you i will flag you immediately 
-//contact me wa.me/255697608274
+//contact me wa.me/256753959924
 
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
-global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernomer = "256753959924"
+global.ownername = "Marvo"
+global.ownernumber = '256753959924'  //owner number 2
 //put your phone number above
-//let the owner as Danny or you can just put your name
+//let the owner as Marvo or you can just put your name
 
 //custom prefix
 global.prefa = [',','!','.','#','&']
@@ -26,11 +26,9 @@ global.anti91 = false //auto block +91
 global.autoswview = true //auto view status/story
 global.antilink = true; // Default: disabled
 global.autostatusReact = true; // Default: disabled
-global.reactEmoji = '😂'; // Default reaction emoji
+global.reactEmoji = '🤪'; // Default reaction emoji
 //Thank you for using 𝓒𝓡𝓔𝓔𝓟𝓨_𝓜𝓓-𝓥𝟏
-//suppoet us by following our official channel
-//https://whatsapp.com/channel/0029VacQFw65Ui2gGv0Kwk1r
-//𝓬𝓻𝓮𝓪𝓽𝓮𝓭 𝓫𝔂 𝒟𝒶𝓃𝓃𝓎
+
 
 
 
@@ -54,25 +52,23 @@ global.mess = {
 };
 
 global.ytname = "YT: https://www.youtube.com/@creepy_technology";
-global.socialm = "GitHub: https://github.com/DannyTech20";
-global.location = "Arusha/Tanzania";
-global.ownername = "𝐷𝛥𝛮𝛮𝑌";
-global.botname = "CREEPY_MD-V1"; 
+global.socialm = "GitHub: https://github.com/Marvooffy";
+global.location = "Arusha/Uganda";
+global.ownername =Marvo Global.botname = "CREEPY_MD-V1"; 
 
 // Sticker details
 global.packname = "Creepy-Tech";
-global.author = "Danny";
+global.author = "Marvo;
 
 // Console view/theme
 global.themeemoji = "🤖";
-global.wm = "Danny";
-
+global.wm = "Marvo"
 global.link = "https://whatsapp.com/channel/0029VacQFw65Ui2gGv0Kwk1r";
 
 global.typemenu = "v1";
 
 global.xbugtex = {
-    xtxt: "Danny🤖",
+    xtxt: "Marvo🤖",
 };
 
 
